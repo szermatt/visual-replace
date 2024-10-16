711b6098051b0345efe8814754effea5ba2472c9
Stephane Zermatten <szermatt@gmx.net>
2024-10-16 12:28:00 +0300

    chore: Add copyright 2020-2024 to every source file.

--- visual-replace.el
+++ visual-replace.el
@@ -1,6 +1,6 @@
 ;;; visual-replace.el --- A prompt for replace-string and query-replace -*- lexical-binding: t -*-
 
-;; Copyright (C) 2020 Stephane Zermatten
+;; Copyright (C) 2020-2024 Stephane Zermatten
 
 ;; Author: Stephane Zermatten <szermatt@gmail.com>
 ;; Maintainer: Stephane Zermatten <szermatt@gmail.com>
@@ -9,6 +9,19 @@
 ;; URL: http://github.com/szermatt/visual-replace
 ;; Package-Requires: ((emacs "26.1"))
 
+;; This program is free software: you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2 of the
+;; License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see
+;; `http://www.gnu.org/licenses/'.
 
 ;;; Commentary:
 ;;
