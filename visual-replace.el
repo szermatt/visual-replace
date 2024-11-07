szermatt/visual-replace

commit e3dc8f436092b216f33bcb71a2da862f4ec9e4a1

feat: When the point is moved during preview, stay there.
Before this change, Visual Replace would always go back to the position
it was originally called from. This could be jarring in the cases where
the point was moved during preview to find the first match, as the
effect of replacement wouldn't be visible.

With this change, the point stays where it was moved to during preview.
A mark is pushed to go back to the original position if necessary.

This change introduces a noticeable change in the way Visual Replace
behaves. To go back to original behavior, set the customization option
visual-replace-keep-initial-position to t.

visual-replace.el
@@ -130,6 +130,16 @@ the result in the prompt, just before the arrow."
   :type 'boolean
   :group 'visual-replace)
 
+(defcustom visual-replace-keep-initial-position nil
+  "If non-nil, always go back to the point `visual-replace' was called from.
+
+If nil the point stays where it was moved in preview mode, by
+commands such as with `visual-replace-next-match'. A mark is
+pushed at the original position to go back to with
+`exchange-point-and-mark', if necessary."
+  :type 'boolean
+  :group 'visual-replace)
+
 (defface visual-replace-match
   '((t :inherit query-replace))
   "How to display the string that was matched.
@@ -695,6 +705,17 @@ INITIAL-SCOPE is used to initialize the replacement scope,
 used as point for \\='from-point. By default, the scope is
 \\='region if the region is active, or \\='from-point otherwise."
   (barf-if-buffer-read-only)
+  (if visual-replace-keep-initial-position
+      (save-excursion
+        (visual-replace-read--internal initial-args initial-scope))
+    (push-mark nil 'nomsg)
+    (visual-replace-read--internal initial-args initial-scope)))
+
+(defun visual-replace-read--internal (&optional initial-args initial-scope)
+  "Private implementation of `visual-replace-read'.
+
+See `visual-replace-read' for a description of the behavior of
+this function and of INITIAL-ARGS and INITIAL-SCOPE."
   (let ((history-add-new-input nil)
         (visual-replace--calling-buffer (current-buffer))
         (visual-replace--calling-window (selected-window))
@@ -719,59 +740,58 @@ used as point for \\='from-point. By default, the scope is
     (setq default-value (car minibuffer-history))
     (when visual-replace--incomplete
       (push visual-replace--incomplete minibuffer-history))
-    (save-excursion
-      (unwind-protect
-          (progn
-            (deactivate-mark)
-            (add-hook 'after-change-functions after-change 0 'local)
-            (when visual-replace-preview
-              (setq timer (run-with-idle-timer
-                           visual-replace-preview-delay
-                           'repeat #'visual-replace--update-preview)))
-            (minibuffer-with-setup-hook
-                (lambda ()
-                  (setq visual-replace--total-ov
-                        (when visual-replace-display-total
-                          (let ((ov (make-overlay (point-min) (point-min))))
-                            (overlay-put ov 'face 'visual-replace-total)
-                            ov)))
-                  (when visual-replace-keep-incomplete
-                    (add-hook 'after-change-functions #'visual-replace--after-change 0 'local))
-                  (setq visual-replace--minibuffer (current-buffer))
-                  (visual-replace-minibuffer-mode t)
-                  (unless initial-args
-                    (run-hooks 'visual-replace-defaults-hook))
-                  (when trigger
-                    (let ((mapping
-                           ;; Emacs 26 lookup-key cannot take a list
-                           ;; of keymaps, using this code for backward
-                           ;; compatibility.
-                           (catch 'has-binding
-                             (dolist (map (current-active-maps))
-                               (let ((func (lookup-key map trigger)))
-                                 (when (functionp func)
-                                   (throw 'has-binding func)))))))
-                      (when (or (eq mapping #'visual-replace)
-                                (eq (command-remapping mapping) #'visual-replace))
-                        (local-set-key trigger visual-replace-secondary-mode-map))))
-                  (visual-replace--show-scope)
-                  (setq-local yank-excluded-properties (append '(separator display face) yank-excluded-properties))
-                  (setq-local text-property-default-nonsticky
-                              (append '((separator . t) (face . t))
-                                      text-property-default-nonsticky)))
-              (setq text (read-from-minibuffer
-                          (concat "Replace "
-                                  (visual-replace--scope-text)
-                                  (if default-value (format " [%s]" default-value) "")
+    (unwind-protect
+        (progn
+          (deactivate-mark)
+          (add-hook 'after-change-functions after-change 0 'local)
+          (when visual-replace-preview
+            (setq timer (run-with-idle-timer
+                         visual-replace-preview-delay
+                         'repeat #'visual-replace--update-preview)))
+          (minibuffer-with-setup-hook
+              (lambda ()
+                (setq visual-replace--total-ov
+                      (when visual-replace-display-total
+                        (let ((ov (make-overlay (point-min) (point-min))))
+                          (overlay-put ov 'face 'visual-replace-total)
+                          ov)))
+                (when visual-replace-keep-incomplete
+                  (add-hook 'after-change-functions #'visual-replace--after-change 0 'local))
+                (setq visual-replace--minibuffer (current-buffer))
+                (visual-replace-minibuffer-mode t)
+                (unless initial-args
+                  (run-hooks 'visual-replace-defaults-hook))
+                (when trigger
+                  (let ((mapping
+                         ;; Emacs 26 lookup-key cannot take a list
+                         ;; of keymaps, using this code for backward
+                         ;; compatibility.
+                         (catch 'has-binding
+                           (dolist (map (current-active-maps))
+                             (let ((func (lookup-key map trigger)))
+                               (when (functionp func)
+                                 (throw 'has-binding func)))))))
+                    (when (or (eq mapping #'visual-replace)
+                              (eq (command-remapping mapping) #'visual-replace))
+                      (local-set-key trigger visual-replace-secondary-mode-map))))
+                (visual-replace--show-scope)
+                (setq-local yank-excluded-properties (append '(separator display face) yank-excluded-properties))
+                (setq-local text-property-default-nonsticky
+                            (append '((separator . t) (face . t))
+                                    text-property-default-nonsticky)))
+            (setq text (read-from-minibuffer
+                        (concat "Replace "
+                                (visual-replace--scope-text)
+                                (if default-value (format " [%s]" default-value) "")
                                 ": ")
-                          initial-input nil nil nil (car search-ring) t))))
-        ;; unwind
-        (remove-hook 'after-change-functions after-change 'local)
-        (visual-replace--reset-idle-search-timer)
-        (when timer
-          (cancel-timer timer))
-        (visual-replace--clear-scope)
-        (visual-replace--clear-preview)))
+                        initial-input nil nil nil (car search-ring) t))))
+      ;; unwind
+      (remove-hook 'after-change-functions after-change 'local)
+      (visual-replace--reset-idle-search-timer)
+      (when timer
+        (cancel-timer timer))
+      (visual-replace--clear-scope)
+      (visual-replace--clear-preview))
     (unless quit-flag (setq visual-replace--incomplete nil))
     (let* ((final-args (visual-replace-args--from-text text))
            (from (visual-replace-args-from final-args))
@@ -834,15 +854,19 @@ of (start . end) as returned by `region-bounds'."
                    (unless (eq arg 'bounds)
                      (error "unsupported: (funcall region-extract-function %s)" arg))
                    (visual-replace--ranges-fix ranges))))
-            (perform-replace
-             from
-             (query-replace-compile-replacement
-              (visual-replace-args-to args)
-              (visual-replace-args-regexp args))
-             (visual-replace-args-query args)
-             (visual-replace-args-regexp args)
-             (visual-replace-args-word args)
-             1 nil start end nil noncontiguous-p))
+            (cl-letf (((symbol-function 'push-mark) (lambda (&rest _args))))
+              ;; perform-replace sets the mark at an uninteresting
+              ;; position. Redefining push-mark avoids that.
+
+              (perform-replace
+               from
+               (query-replace-compile-replacement
+                (visual-replace-args-to args)
+                (visual-replace-args-regexp args))
+               (visual-replace-args-query args)
+               (visual-replace-args-regexp args)
+               (visual-replace-args-word args)
+               1 nil start end nil noncontiguous-p)))
           (goto-char origin))
       (set-marker origin nil))))
 

test/visual-replace-test-helper.el
@@ -45,6 +45,7 @@ are triggered by the key F1 ! when running test-visual-replace-run.")
            (visual-replace-highlight-match-at-point nil)
            (visual-replace-minibuffer-mode-hook nil)
            (visual-replace-defaults-hook nil)
+           (visual-replace-keep-initial-position t)
            (test-visual-replace-snapshot nil))
        (cl-letf (((symbol-function 'sit-for) (lambda (_))))
          (ert-with-test-buffer nil

test/visual-replace-test.el
@@ -412,6 +412,65 @@
        ;; We're now back at the original position.
        (should (equal (- (line-number-at-pos (point-max)) 3) (line-number-at-pos (point))))))))
 
+(ert-deftest test-visual-replace-restore-position-after-jump ()
+  (test-visual-replace-env
+   (with-selected-window (display-buffer (current-buffer))
+     (let* ((snapshots)
+            (win (selected-window))
+            (height (window-height win))
+            (to-replace (* 2 height)))
+       (dotimes (i (* 3 height))
+         (insert (format "this is text %d.\n" i)))
+       (goto-char (point-min))
+       (forward-line 3)
+       (recenter)
+       (define-key
+        visual-replace-mode-map
+        (kbd "C-c t")
+        (lambda ()
+          (interactive)
+          (visual-replace--update-preview)
+          (while visual-replace--idle-search-timer
+            (cl-assert (memq visual-replace--idle-search-timer timer-idle-list))
+            (ert-run-idle-timers))))
+       (visual-replace-ert-simulate-keys (kbd (format "t e x t SPC %d . TAB r e p l C-c t RET" to-replace))
+         (call-interactively 'visual-replace))
+
+       (should (equal "this is text 3."
+                      (buffer-substring-no-properties
+                       (line-beginning-position)
+                       (line-end-position))))))))
+
+(ert-deftest test-visual-replace-stay-in-position-after-jump ()
+  (test-visual-replace-env
+     (with-selected-window (display-buffer (current-buffer))
+       (let* ((snapshots)
+              (win (selected-window))
+              (visual-replace-keep-initial-position nil)
+              (height (window-height win))
+              (to-replace (* 2 height)))
+         (dotimes (i (* 3 height))
+           (insert (format "this is text %d.\n" i)))
+         (goto-char (point-min))
+         (forward-line 3)
+         (recenter)
+         (define-key
+          visual-replace-mode-map
+          (kbd "C-c t")
+          (lambda ()
+            (interactive)
+            (visual-replace--update-preview)
+            (while visual-replace--idle-search-timer
+              (cl-assert (memq visual-replace--idle-search-timer timer-idle-list))
+              (ert-run-idle-timers))))
+         (visual-replace-ert-simulate-keys (kbd (format "t e x t SPC %d . TAB r e p l C-c t RET" to-replace))
+                                           (call-interactively 'visual-replace))
+         ;; The point should be at the first match.
+         (should (equal "this is repl"
+                        (buffer-substring-no-properties
+                         (line-beginning-position)
+                         (line-end-position))))))))
+
 (ert-deftest test-visual-replace-small-ranges ()
   (ert-with-test-buffer nil
     (dotimes (i 300)

visual-replace.texi
@@ -366,6 +366,7 @@ also moving through the matches with @code{<down>} or @code{<up>}.
 @subsection Customization
 
 
+@geindex variable; visual-replace-keep-initial-position
 @geindex variable; visual-replace-display-total
 @geindex variable; visual-replace-preview
 @geindex variable; visual-replace-first-match
@@ -394,6 +395,16 @@ their replacements. This is enabled by default.
 option enabled, Visual Replace always tries to have at least one
 match visible in the preview, even if it means jumping to another
 section of the buffer. This is enabled by default.
+
+@item 
+@code{M-x customize-option keep-initial-position} With this option
+enabled, Visual Replace goes back to the point it was called from,
+even if the point was moved during preview, to display the first
+match, or manually with @code{<down>} or @code{<up>}.
+
+Note that in the case where the point is moved during preview,
+Visual Replace sets a mark at the original location, to go back too
+if necessary.
 
 @item 
 @code{M-x customize-option visual-replace-display-total} By default,

commit 4c3a16c442834546e0cb6227817c7a35410ffb5f

fix: Fix replace in region, broken by e3dc8f4.

visual-replace.el
@@ -707,11 +707,10 @@ used as point for \\='from-point. By default, the scope is
   (barf-if-buffer-read-only)
   (if visual-replace-keep-initial-position
       (save-excursion
-        (visual-replace-read--internal initial-args initial-scope))
-    (push-mark nil 'nomsg)
-    (visual-replace-read--internal initial-args initial-scope)))
+        (visual-replace-read--internal initial-args initial-scope nil))
+    (visual-replace-read--internal initial-args initial-scope 'push-mark)))
 
-(defun visual-replace-read--internal (&optional initial-args initial-scope)
+(defun visual-replace-read--internal (&optional initial-args initial-scope push-mark)
   "Private implementation of `visual-replace-read'.
 
 See `visual-replace-read' for a description of the behavior of
@@ -740,6 +739,8 @@ this function and of INITIAL-ARGS and INITIAL-SCOPE."
     (setq default-value (car minibuffer-history))
     (when visual-replace--incomplete
       (push visual-replace--incomplete minibuffer-history))
+    (when push-mark
+      (push-mark nil 'nomsg))
     (unwind-protect
         (progn
           (deactivate-mark)