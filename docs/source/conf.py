d9a89fa02c6170a1b417b3e71e85d481213f318b
Stephane Zermatten <szermatt@gmx.net>
2024-11-18 22:04:55 +0200

    Release visual-replace 1.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,8 @@ project = 'visual-replace'
 copyright = '2020-2024, Stephane Zermatten'
 author = 'Stephane Zermatten'
 
-release = '1.0.1snapshot'
-version = '1.0.1snapshot'
+release = '1.1'
+version = '1.1'
 
 # -- General configuration
 
