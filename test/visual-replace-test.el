04da84c74b8f930d06e89944120191d35c4e35d4
Stephane Zermatten <szermatt@gmx.net>
2024-12-25 16:14:15 +0200

    test: Remove test-visual-replace-run in visual-replace-test.el
    Uses of this macro were not really justified; these tests are much more
    readable if they make the visual-replace-args instance themselves.

--- test/visual-replace-test.el
+++ test/visual-replace-test.el
@@ -41,8 +41,8 @@
    (insert "hello 1\n")
    (save-excursion ;; point is here
      (insert "hello 2\n"))
-   (test-visual-replace-run "hello RET hullo RET"
-                        (call-interactively 'visual-replace))
+   (visual-replace (visual-replace-make-args :from "hello" :to "hullo")
+                     `((,(point) . ,(point-max))))
    (should (equal (test-visual-replace-content)
                    "hello 1\nhullo 2\n"))))
 
@@ -51,8 +51,8 @@
    (insert "hello 1\n")
    (save-excursion ;; point is here
      (insert "hello 2\n"))
-   (test-visual-replace-run "hello RET hullo <F1> s RET"
-                        (call-interactively 'visual-replace))
+   (visual-replace (visual-replace-make-args :from "hello" :to "hullo")
+                     `((,(point-min) . ,(point-max))))
    (should (equal (test-visual-replace-content)
                    "hullo 1\nhullo 2\n"))))
 
@@ -64,12 +64,11 @@
      (insert "hello 2\n")
      (setq mark-2 (point))
      (insert "hello 3\n")
-     (goto-char mark-1)
-     (set-mark mark-2))
-   (test-visual-replace-run "hello RET hullo RET"
-                        (call-interactively 'visual-replace))
+
+     (visual-replace (visual-replace-make-args :from "hello" :to "hullo")
+                     `((,mark-1 . ,mark-2)))
    (should (equal (test-visual-replace-content)
-                   "hello 1\nhullo 2\nhello 3\n"))))
+                   "hello 1\nhullo 2\nhello 3\n")))))
 
 (ert-deftest test-visual-replace-in-region-keep-initial-position ()
   (test-visual-replace-env
@@ -80,19 +79,24 @@
        (insert "hello 2\n")
        (setq mark-2 (point))
        (insert "hello 3\n")
-       (goto-char mark-1)
-       (set-mark mark-2))
-     (test-visual-replace-run "hello RET hullo RET"
-                              (call-interactively 'visual-replace))
-     (should (equal (test-visual-replace-content)
-                    "hello 1\nhullo 2\nhello 3\n")))))
+       (goto-char mark-2)
+
+       (visual-replace (visual-replace-make-args :from "hello" :to "hullo")
+                       `((,mark-1 . ,mark-2)))
+       (should (equal "hello 1\nhullo 2\nhello 3\n"
+                      (buffer-string)))
+
+       (should (equal mark-2 (point)))))))
 
 (ert-deftest test-visual-replace-regexp ()
   (test-visual-replace-env
    (insert "hello, world")
    (goto-char (point-min))
-   (test-visual-replace-run "h\\(.*?\\)o RET H\\1O <F1> r RET"
-                        (call-interactively 'visual-replace))
+   (visual-replace (visual-replace-make-args
+                    :from "h\\(.*?\\)o"
+                    :to "H\\1O"
+                    :regexp t)
+                   `((,(point-min) . ,(point-max))))
    (should (equal (test-visual-replace-content)
                    "HellO, world"))))
 
@@ -100,9 +104,9 @@
   (test-visual-replace-env
    (insert "hello, hello")
    (goto-char (point-min))
-   (test-visual-replace-run
-    "h\\(.*?\\)o RET \\#\\,(upcase SPC \\1)e <F1> r RET"
-    (call-interactively 'visual-replace))
+   (visual-replace (visual-replace-make-args
+                    :from "h\\(.*?\\)o" :to "\\#\\,(upcase \\1)e" :regexp t)
+                   `((,(point-min) . ,(point-max))))
    (should (equal (test-visual-replace-content)
                    "0ELLe, 1ELLe"))))
 
@@ -112,27 +116,31 @@
    (insert "hello 2\n")
    (insert "hello 3\n")
    (goto-char (point-min))
-   (test-visual-replace-run "hello RET hullo <F1> q RET n y n"
-                        (call-interactively 'visual-replace))
-   (should (equal (test-visual-replace-content)
-                   "hello 1\nhullo 2\nhello 3\n"))))
+
+   (ert-simulate-keys (kbd "n y n")
+    (visual-replace (visual-replace-make-args :from "hello" :to "hullo" :query t)
+                    `((,(point-min) . ,(point-max)))))
+
+   (should (equal "hello 1\nhullo 2\nhello 3\n"
+                  (buffer-string)))))
 
 (ert-deftest test-visual-replace-lax-ws ()
   (test-visual-replace-env
    (insert "hello   world.")
    (goto-char (point-min))
-   (test-visual-replace-run "hello SPC world TAB foobar <F1> l RET"
-                        (call-interactively 'visual-replace))
+   (visual-replace (visual-replace-make-args
+                    :from "hello world" :to "foobar" :lax-ws-non-regexp t)
+                   `((,(point-min) . ,(point-max))))
    (should (equal (test-visual-replace-content) "foobar."))))
 
 (ert-deftest test-visual-replace-lax-ws-regexp ()
   (test-visual-replace-env
    (insert "hello world.")
    (goto-char (point-min))
-   (test-visual-replace-run "hello SPC w.*d TAB foobar <F1> r <F1> l RET"
-                        (call-interactively 'visual-replace))
+   (visual-replace (visual-replace-make-args
+                    :from "hello w.*d" :to "foobar" :lax-ws-regexp t :regexp t)
+                   `((,(point-min) . ,(point-max))))
    (should (equal (test-visual-replace-content) "foobar."))))
-
 
 (ert-deftest test-visual-replace-preprocess ()
   (test-visual-replace-env
@@ -142,8 +150,11 @@
                         (concat "^" (visual-replace-args-from args)))))))
      (insert "hello, hello")
      (goto-char (point-min))
-     (test-visual-replace-run "hello RET hullo <F1> r RET"
-                              (call-interactively 'visual-replace))
+
+     (visual-replace (visual-replace-make-args
+                      :from "hello" :to "hullo" :regexp t)
+                     `((,(point-min) . ,(point-max))))
+
      (should (equal (test-visual-replace-content)
                     "hullo, hello")))))
 
